5cd2a1afbb5069288bbf82d093d2593c0c6ea0ee
Alberto Di Savia Puglisi <disalberto@gmail.com>
2020-05-02 12:05:58 +0200

    made config script a bit more generig

--- config/set_config.sh
+++ config/set_config.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Updating configuration file..."
-cp /home/disalberto/.config/spotify/Users/alberto88a-user/prefs /home/disalberto/.config/spotify/Users/alberto88a-user/prefs.backup
+# USAGE: 
+# bash set_config.sh source_config dest_config
 
-cp -f ./alberto88a-user.prefs /home/disalberto/.config/spotify/Users/alberto88a-user/prefs
+
+backup_and_save() {
+  echo "Updating configuration file..."
+  cp $2 $2.backup
+  cp -f $1 $2
+}
+
+
+echo "#1: Spotify"
+backup_and_save ./spotify/alberto88a-user.prefs /home/disalberto/.config/spotify/Users/alberto88a-user/prefs
